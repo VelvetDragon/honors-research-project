2cc8c5c65b247eb73e1dde9eff6a446c50b9645d
Suwan Aryal <aryalsuwan.2003@gmail.com>
2024-11-20 20:55:58 -0600

    pages tested

---
+++ src/app/conclusion/page.js
@@ -0,0 +1,14 @@
+// src/app/conclusion/page.js
+'use client'
+
+import styles from '@/styles/Conclusion.module.css';
+import AnimatedSection from '@/components/AnimatedSection';
+import Image from 'next/image';
+
+
+
+export default function conclusion() {
+    return (
+        <div>Yes boss</div>
+    )
+}
---
+++ src/app/findings-and-analysis/page.js
@@ -0,0 +1,12 @@
+// src/app/methodology/page.js
+import styles from '@/styles/FindingsAndAnalysis.module.css';
+import AnimatedSection from '@/components/AnimatedSection';
+import Image from 'next/image';
+
+
+
+export default function findingsandanalysis() {
+  return (
+    <div>My findings </div>
+  )
+}
